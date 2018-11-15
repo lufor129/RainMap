557e7645ca9f236a38b0419459237fcccd9d0fa6
lufor129 <s59654655@gmail.com>
2018-11-15 20:40:19 +0800

    set environment

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-  baseUrl: '/RainMap'
+  baseUrl: '/RainMap/',
+  outputDir: undefined,
+  assetsDir: undefined,
+  runtimeCompiler: undefined,
+  productionSourceMap: undefined,
+  parallel: undefined,
+  css: undefined
 }
